ecc646652a14f6f8e6430b161807c6400fda0b38
braydie <braydie.grove@gmail.com>
2014-12-15 12:15:55 +0000

    Adding nunit project generator to github

---
+++ NUnit Project Generator/Options.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace NUnitProjectGenerator
+{
+    internal class Options
+    {
+        [Option('d', "directory", Required = true, 
+            HelpText = "Full path to the root of the directory to search")]
+        public string TestRootDirectory { get; set; }
+
+        [Option('e', "exclude", Required = false, 
+            HelpText = "Optional argument to exclude folder")]
+        public string FolderToExclude { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+    }
+}
---
+++ NUnit Project Generator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace NUnitProjectGenerator
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            var Options = new Options();
+            if(CommandLine.Parser.Default.ParseArguments(Args, Options))
+            {                
+                var Dlls = Helpers.GetTestAssemblies(Options.TestRootDirectory);
+
+                if(!String.IsNullOrEmpty(Options.FolderToExclude))
+                {
+                    Dlls = Dlls.Where(x => !x.Contains(Options.FolderToExclude));
+                }
+                Helpers.CreateProjectFile(Dlls, Options.TestRootDirectory);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid command line arguments");
+            }            
+        }        
+    }
+}
